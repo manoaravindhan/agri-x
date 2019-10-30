4737ecaa4883b8c796dc3031ce8dcc0e36f05a6d
Divakar Kumar <divakar.kumar@kdi.kongsberg.com>
2019-10-31 01:10:27 +0530

    Updates

---
+++ precache-manifest.427ada9e1084444da1b7e76569652612.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e07effda0a3c66198f88be55f1920741",
+    "url": "/manoaravindhan/agri-x/index.html"
+  },
+  {
+    "revision": "7e376a5a54e3a38ebb42",
+    "url": "/manoaravindhan/agri-x/static/css/main.c219c1a8.chunk.css"
+  },
+  {
+    "revision": "8df2caf63352ae1f0b07",
+    "url": "/manoaravindhan/agri-x/static/js/2.16756b13.chunk.js"
+  },
+  {
+    "revision": "7e376a5a54e3a38ebb42",
+    "url": "/manoaravindhan/agri-x/static/js/main.559f3841.chunk.js"
+  },
+  {
+    "revision": "19d193dc01dcf5352569",
+    "url": "/manoaravindhan/agri-x/static/js/runtime-main.2fe4e974.js"
+  }
+]);
